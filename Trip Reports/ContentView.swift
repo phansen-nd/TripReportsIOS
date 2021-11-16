02eb36c1c3f765b983e4700648d36194ea63256c
Pat Hansen <patrick.hansen1618@gmail.com>
2021-11-15 20:53:00 -0800

    Initial Commit

---
+++ Trip Reports/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trip Reports
+//
+//  Created by Pat Hansen on 11/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Trip Reports/Trip_ReportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trip_ReportsApp.swift
+//  Trip Reports
+//
+//  Created by Pat Hansen on 11/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Trip_ReportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
